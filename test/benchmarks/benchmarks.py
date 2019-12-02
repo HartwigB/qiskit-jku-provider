a41967de92e369175ef64fadbb6f7304f4d518f6
HartwigB <bauer.hartwig@gmx.at>
2019-12-02 10:15:45 +0100

    edited asv benchmark

--- test/benchmarks/benchmarks.py
+++ test/benchmarks/benchmarks.py
@@ -19,32 +19,25 @@ from qiskit_jku_provider import QasmSimulator
 
 class QasmSimulatorJKUBenchmarkSuite:
     """Runs the Basic qasm_simulator tests from Terra on JKU."""
-
-    def setup(self):
-        QASM_FILES    = ["3_17_13.qasm", "4_49_16.qasm", "4gt4-v0_72.qasm"]
-        self.seed     = 88
-        self.backend  = QasmSimulator(silent=True)
-        self.circuits = []
-        for file in QASM_FILES:
-            full_file             = os.path.join(os.path.dirname(__file__), 'qasms', file)
-            compiled_circuit      = QuantumCircuit.from_qasm_file(full_file)
-            compiled_circuit.name = file
-            self.circuits.append(compiled_circuit)
-        self.circuit = compiled_circuit
+    params = ["3_17_13.qasm", "4_49_16.qasm", "4gt4-v0_72.qasm"]
+    def setup(self, filename):
+        self.seed    = 88
+        self.backend = QasmSimulator(silent=True)
+        full_file    = os.path.join(os.path.dirname(__file__), 'qasms', filename)
+        self.circuit = QuantumCircuit.from_qasm_file(full_file)
+        self.circuit.name = filename
+           
 
     def time_qasm_simulator_single_shot(self):
         """Test single shot run."""
-        for circuit in self.circuits:
-            result = execute(circuit, self.backend, seed_transpiler=34342, shots=1).result()
-            self.assertEqual(result.success, True)
+        result = execute(self.circuit, self.backend, seed_transpiler=34342, shots=1).result()
+        self.assertEqual(result.success, True)
 
     def time_qasm_simulator(self):
         """Test data counts output for single circuit run against reference."""
         shots = 1024
-        for circuit in self.circuits:
-            result = execute(circuit, self.backend, seed_transpiler=34342, shots=shots).result()
-            result = execute(circuit, self.backend, seed_transpiler=34342, shots=shots).result()
-            self.assertEqual(result.success, True)
+        result = execute(self.circuit, self.backend, seed_transpiler=34342, shots=shots).result()
+        self.assertEqual(result.success, True)
             #threshold = 0.04 * shots
             #counts = result.get_counts('test')
             #target = {'100 100': shots / 8, '011 011': shots / 8,
